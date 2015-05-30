fa3e8ac5276615bb980c300179f7b7db1bdb147e
MISHIMA Hiroyuki <missy@be.to>
2015-05-30 23:47:56 +0900

    use Dockerfile of inutano/virtuoso

--- readme.md
+++ readme.md
@@ -1,9 +1,16 @@
 # Dockerfile: virtuoso-go-hpo
 A dockerfile to download Gene Ontology (GO), Human Phenotype Ontology (HPO), and HPO Annotation (HPA) followed by importing them into Virtuoso
 
+# Build
+At the directory containing the Dockerfile file:
+```bash
+$ sudo docker build -build -t virtuoso-go-hpo .
+```
+
+# Run
 ```bash
 $ sudo docker run -d -p 8890:8890 --name virtuoso inutano/virtuoso
-$ sudo docker run -it --link virtuoso:virtuoso -v ${HOME}/samples:/opt/Ontologies/samples misshie/virtuoso-go-hpo
+$ sudo docker run -it --link virtuoso:virtuoso -v ${HOME}/samples:/opt/Ontologies/samples virtuoso-go-hpo
 $ curl \
      --form "query=SELECT COUNT(?s) FROM <http://purl.obolibrary.org/obo/go/go.owl> WHERE { ?s ?p ?o . }" \
      http://localhost:8890/sparql
--- scripts/virtuoso-import.sh
+++ scripts/virtuoso-import.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 
+echo
+echo ">>> Upload HPO into Virtuoso"
 curl --verbose --digest -u dba:dba \
     -T /opt/Ontologies/HPO/hp.owl \
     --url http://virtuoso:8890/sparql-graph-crud-auth?graph-uri=http://purl.obolibrary.org/obo/hp.owl
+echo
+echo ">>> Upload GO into Virtuoso"
 curl --verbose --digest -u dba:dba \
     -T /opt/Ontologies/GO/go.owl \
     --url http://virtuoso:8890/sparql-graph-crud-auth?graph-uri=http://purl.obolibrary.org/obo/go/go.owl
 
-ruby /opt/Ontologies/scripts/hpa2rdf/hpa2rdf.rb \
-    /opt/Ontologies/HPA/phenotype_annotation.tab \
-    > /opt/Ontologies/HPA/phenotype_annotation.ttl
-curl --verbose --digest -u dba:dba \
-    -T /opt/Ontologies/HPA/phenotype_annotation.ttl \
-    --url http://virtuoso:8890/sparql-graph-crud-auth?graph-uri=http://misshie.jp/rdf/phenotype_annotation.ttl
-
+echo
+echo ">>> Convert phenotype_annotation_hpoteam.tab to Turtle"
 ruby /opt/Ontologies/scripts/hpa2rdf/hpa2rdf.rb \
     /opt/Ontologies/HPA/phenotype_annotation_hpoteam.tab \
     > /opt/Ontologies/HPA/phenotype_annotation_hpoteam.ttl
+wc -l /opt/Ontologies/HPA/phenotype_annotation_hpoteam.ttl
+echo
+echo ">>> Upload phenotype_annotation_hpoteam.ttl into Virtuoso"
 curl --verbose --digest -u dba:dba \
     -T /opt/Ontologies/HPA/phenotype_annotation_hpoteam.ttl \
     --url http://virtuoso:8890/sparql-graph-crud-auth?graph-uri=http://misshie.jp/rdf/phenotype_annotation_hpoteam.ttl
 
+echo
+echo ">>> Convert phenotype_annotation.tab to Turtle"
+ruby /opt/Ontologies/scripts/hpa2rdf/hpa2rdf.rb \
+    /opt/Ontologies/HPA/phenotype_annotation.tab \
+    > /opt/Ontologies/HPA/phenotype_annotation.ttl
+wc -l /opt/Ontologies/HPA/phenotype_annotation.ttl
+echo
+echo ">>> Upload phenotype_annotation.ttl into Virtuoso"
+curl --verbose --digest -u dba:dba \
+    -T /opt/Ontologies/HPA/phenotype_annotation.ttl \
+    --url http://virtuoso:8890/sparql-graph-crud-auth?graph-uri=http://misshie.jp/rdf/phenotype_annotation.ttl
+
+echo
+echo ">>> Convert negative_phenotype_annotation.tab to Turtle"
 ruby /opt/Ontologies/scripts/hpa2rdf/hpa2rdf.rb \
     /opt/Ontologies/HPA/negative_phenotype_annotation.tab \
     > /opt/Ontologies/HPA/negative_phenotype_annotation.ttl
+wc -l /opt/Ontologies/HPA/negative_phenotype_annotation.ttl
+echo
+echo ">>> Upload negative_phenotype_annotation.ttl into Virtuoso"
 curl --verbose --digest -u dba:dba \
     -T /opt/Ontologies/HPA/negative_phenotype_annotation.ttl \
     --url http://virtuoso:8890/sparql-graph-crud-auth?graph-uri=http://misshie.jp/rdf/negative_phenotype_annotation.ttl
