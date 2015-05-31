fcdcf8e5a3c54ccbfa929219a8301900503b77ce
MISHIMA Hiroyuki <missy@be.to>
2015-05-31 18:46:26 +0900

    import all three turtle files

--- scripts/import.sql
+++ scripts/import.sql
@@ -11,8 +11,18 @@ DB.DBA.RDF_LOAD_RDFXML_MT
 'http://purl.obolibrary.org/obo/go/go.owl');
 
 DB.DBA.TTLP_MT
-(file_to_string_output('/usr/local/virtuoso-opensource/var/lib/virtuoso/db/Phenotype_annotation_hpoteam.ttl),
+(file_to_string_output('/usr/local/virtuoso-opensource/var/lib/virtuoso/db/phenotype_annotation_hpoteam.ttl'),
 '',
 'http://misshie.jp/rdf/phenotype_annotation_hpoteam.ttl');
+
+DB.DBA.TTLP_MT
+(file_to_string_output('/usr/local/virtuoso-opensource/var/lib/virtuoso/db/phenotype_annotation.ttl'),
+'',
+'http://misshie.jp/rdf/phenotype_annotation.ttl');
+
+DB.DBA.TTLP_MT
+(file_to_string_output('/usr/local/virtuoso-opensource/var/lib/virtuoso/db/negative_phenotype_annotation.ttl'),
+'',
+'http://misshie.jp/rdfnegative_/phenotype_annotation.ttl');
 
 EXIT;
